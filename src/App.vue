c4fcb29452342155dd871807e9e655aff8cd52c4
valenmelo002 <valentinaelo02@gmail.com>
2025-06-21 11:50:24 -0500

    Creacion de app vue, con nombre de doctores veterinarios

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-app-bar app flat color="primary" density="comfortable">
-      <v-toolbar-title class="text-white">Gestión de Especialistas</v-toolbar-title>
+      <v-toolbar-title class="text-white">Gestión de doctores veterinarios</v-toolbar-title>
     </v-app-bar>
 
     <v-main class="bg-grey-lighten-4 fill-height">
